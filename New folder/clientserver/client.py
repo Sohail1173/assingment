a8041b28a737fc433724c42f7583fc71c1431e04
MD SUHAIL AHMED <116140284+Sohail1173@users.noreply.github.com>
2023-03-27 13:24:39 -0400

    Update client.py

--- New folder/clientserver/client.py
+++ New folder/clientserver/client.py
@@ -1,14 +1,14 @@
 import socket
-#Create a socket instance
 socketObject = socket.socket()
-#Using the socket connect to a server...in this case localhost
+#Now socket is created and it has to  working as a client
+#now i will try to connect with the server side
 socketObject.connect(("localhost", 35491))
 print("Connected to localhost")
-# Send a message to the web server to supply a page as given by Host param of GET request
+
 HTTPMessage = "GET / HTTP/1.1\r\nHost: localhost\r\n Connection: close\r\n\r\n"
 bytes       = str.encode(HTTPMessage)
 socketObject.sendall(bytes)
-# Receive the data
+
 while(True):
     data = socketObject.recv(1024)
     print(data)
