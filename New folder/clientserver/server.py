c72d05c517f79c92b78362e519020c2058efba04
MD SUHAIL AHMED <116140284+Sohail1173@users.noreply.github.com>
2023-03-27 13:40:46 -0400

    Update server.py

--- New folder/clientserver/server.py
+++ New folder/clientserver/server.py
@@ -1,17 +1,11 @@
 import random
-#socket is the library that is used to create the server.
 import socket
-"""next i need to create the object in the socket which is going to be
-working as a server"""
 serverSocket = socket.socket()
-""" binding the server socket with the IP and Port number,can take the port 
-number between 0 to 65000"""
-
+print("Server socket created")
 ip      = "127.0.0.1"
 port    = 35491
 serverSocket.bind((ip, port))
 print("Server socket bound with with ip {} port {}".format(ip, port))
-#serversocket.listen will now ready to accept  the communication from clients
 serverSocket.listen()
 while(True):
     (clientConnection, clientAddress) = serverSocket.accept()
