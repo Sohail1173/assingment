899a460cfbe04505f1a3c8165ee9115520d1ec44
MD SUHAIL AHMED <116140284+Sohail1173@users.noreply.github.com>
2023-03-27 10:41:27 -0400

    Update server.py

--- New folder/clientserver/server.py
+++ New folder/clientserver/server.py
@@ -1,26 +1,18 @@
 import random
-import socketio
-import eventlet
-
-
-sio = socketio.Server()
-app = socketio.WSGIApp(sio)
-
-@sio.event
-def connect(sid, environ):
-    print('Client connected')
-
-@sio.event
-def disconnect(sid):
-    print('Client disconnected')
-
-@sio.event
-def random_value(sid):
-    value = random.randint(1, 100)
-    print('Server generated random value:', value)
-    sio.emit('random_value', value)
-    
-if __name__ == '__main__':
-    port = 5000
-    print('Starting server on port', port)
-    eventlet.wsgi.server(eventlet.listen(('', port)), app)
+import socket
+# Create a server socket
+serverSocket = socket.socket()
+print("Server socket created")
+# Associate the server socket with the IP and Port
+ip      = "127.0.0.1"
+port    = 35491
+serverSocket.bind((ip, port))
+print("Server socket bound with with ip {} port {}".format(ip, port))
+# Make the server listen for incoming connections
+serverSocket.listen()
+# Server incoming connections "one by one"
+while(True):
+    (clientConnection, clientAddress) = serverSocket.accept()
+    randomvalue=random.randint(0,5)
+    print("Accepted {} connections so far".format(randomvalue))
+    # read from client connection
