49c36a6d54d3a94c64235f762efde51c74b9ee95
MD SUHAIL AHMED <116140284+Sohail1173@users.noreply.github.com>
2023-03-27 10:40:32 -0400

    Update client.py

--- New folder/clientserver/client.py
+++ New folder/clientserver/client.py
@@ -1,21 +1,18 @@
-import socketio
-
-sio = socketio.Client()
-
-@sio.event
-def connect():
-    print('Connected to server')
-
-@sio.event
-def random_number(number):
-    print(f'Receied random number: {number}')
-
-@sio.event
-def disconnect():
-    print('Disconnected from server')
-
-if __name__ == '__main__':
-    server_url = 'http://localhost:5000'
-    print(f'Conncting to server at {server_url}')
-    sio.connect(server_url)
-    sio.wait()
+import socket
+#Create a socket instance
+socketObject = socket.socket()
+#Using the socket connect to a server...in this case localhost
+socketObject.connect(("localhost", 35491))
+print("Connected to localhost")
+# Send a message to the web server to supply a page as given by Host param of GET request
+HTTPMessage = "GET / HTTP/1.1\r\nHost: localhost\r\n Connection: close\r\n\r\n"
+bytes       = str.encode(HTTPMessage)
+socketObject.sendall(bytes)
+# Receive the data
+while(True):
+    data = socketObject.recv(1024)
+    print(data)
+    if(data==b''):
+        print("Connection closed")
+        break
+socketObject.close()
